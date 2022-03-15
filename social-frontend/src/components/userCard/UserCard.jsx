2faae20f49864397240bfbce7d2db229a534f48d
aayushsrivastava9760 <81965095+aayushsrivastava9760@users.noreply.github.com>
2022-03-15 07:09:44 +0530

    testing

--- social-frontend/src/components/userCard/UserCard.jsx
+++ social-frontend/src/components/userCard/UserCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './userCard.css'
 
+
 const UserCard = ({user}) => {
   const {username,profilePicture,desc} = user
     return (

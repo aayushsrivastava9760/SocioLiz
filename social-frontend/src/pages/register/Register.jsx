ad1307106037e92e82483d88118ce56b9cecf9c0
aayushsrivastava9760 <81965095+aayushsrivastava9760@users.noreply.github.com>
2022-04-03 07:43:38 +0530

    another fix

--- social-frontend/src/pages/register/Register.jsx
+++ social-frontend/src/pages/register/Register.jsx
@@ -1,7 +1,6 @@
 // import axios from 'axios'
 import React, { useRef } from 'react'
 import './register.css'
-import { useNavigate } from "react-router-dom"
 import {Link} from "react-router-dom"
 import axios from '../../utils/axios'
 
